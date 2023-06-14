8a54ba135e998fb3b6a608bae7cbc663f4ba6edf
Nadhamudin <nadhamudin.ismail@student.moringaschool.com>
2023-06-14 19:20:58 +0300

    Completed the assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 1;
+let num2 = 1;
+let multiply = num1 / num1 * 31 + num2 / num2 * 31;
+let random = Math.round(Math.random());
+
+
+let num3 = 28;
+let num4 = 8;
+
+let mod = num3 % num4;
+
+let max = Math.max(1,2,3,6,20);
 
 // Write your code here
